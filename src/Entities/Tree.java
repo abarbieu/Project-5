4c8a1cc2dcbd930507125a286730814ec77234d2
Aidan <happycamper108@gmail.com>
2019-06-04 22:15:11 -0700

    readded tree entitity

---
+++ src/Entities/Tree.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+import processing.core.PImage;
+
+public class Tree extends Entity {
+    public Tree(String id, List<PImage> images, Point pos) {
+        super(id,pos, images);
+    }
+}
